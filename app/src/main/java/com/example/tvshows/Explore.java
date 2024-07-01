953ca80ce1c16fd4bd8329edcf8aa92501fed85c
tuyenrickchoi <139416392+tuyenrickchoi@users.noreply.github.com>
2024-07-01 16:07:52 +0700

    first commit

---
+++ app/src/main/java/com/example/tvshows/Explore.java
@@ -0,0 +1,15 @@
+package com.example.tvshows;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Explore extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore);
+    }
+}
